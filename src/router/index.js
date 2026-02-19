62c86ffa221682537cc97bed0a904fdfc24545b3
Derrick Best <dbest180@gmail.com>
2026-02-19 17:33:34 -0600

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import WikiView from '../views/WikiView.vue' // This will be our generic page viewer
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // This ensures the router knows we are in a subfolder /azzaum-chronicles/
+  history: createWebHistory(import.meta.env.BASE_URL), 
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      // The :slug part is dynamic. It matches /wiki/javascript, /wiki/css, etc.
-      path: '/wiki/:slug', 
-      name: 'wiki',
-      component: WikiView
-    }
+    { path: '/', component: HomeView },
+    { path: '/wiki/:id', component: () => import('../views/WikiView.vue') }
   ]
 })
 
